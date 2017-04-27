4c83b4c13fb36c03038397ba140c6c4eff46b745
Ted Michael Celis <krazete@gmail.com>
2017-04-26 20:07:46 -0700

    improved keys, tone, and history
    added a key on the top left, replaced gain diminishing loop with
    exponentialRampToValueAtTime, replaced relative timekeeping with
    absolute timekeeping for history, and replaced recursive playback with
    an iterative loop

--- piano.js
+++ piano.js
@@ -1,10 +1,10 @@
 javascript:
+var fo = prompt("Frequency Offset:", 29);
 var piano = {
 	audio: new AudioContext(),
-	initTime: new Date().getTime(),
-	lastTime: 0,
+	initTime: 0,
 	keys: [
-		"Tab", "1", "q", "2", "w", "3", "e", "r", "5", "t", "6", "y",
+		"`", "Tab", "1", "q", "2", "w", "3", "e", "r", "5", "t", "6", "y",
 		"u", "8", "i", "9", "o", "0", "p", "[", "=", "]", "Backspace", "\\",
 		"ShiftLeft", "a", "z", "s", "x", "d", "c", "v", "g", "b", "h", "n",
 		"m", "k", ",", "l", ".", ";", "/", "ShiftRight",
@@ -12,70 +12,47 @@ var piano = {
 	history: [],
 	note: function(e, h) {
 		var hz = this.keys.indexOf(e);
-		if (h) { // should change relative timekeeping to absolute timekeeping
-			var thisTime = new Date().getTime();
-			this.history.push({key: e, time: thisTime - this.lastTime});
-			this.lastTime = thisTime;
-		}
+		if (h)
+			this.history.push({key: e, time: this.audio.currentTime - this.initTime});
 		var vol = this.audio.createGain();
 		vol.connect(this.audio.destination);
 		var osc = this.audio.createOscillator();
-		osc.frequency.value = hz < 0 ? 0 : (440 * Math.pow(2, (hz - 49) / 12));
+		osc.frequency.value = hz < 0 ? 0 : (440 * Math.pow(2, (hz - fo) / 12));
 		osc.type = "triangle";
-
-/* why does this make the audio crackle?
-var wave = this.audio.createPeriodicWave(
-	new Float32Array([0, 1]),
-	new Float32Array([0, 0])
-);
-osc.setPeriodicWave(wave);
-*/
-
 		osc.connect(vol);
 		osc.start();
-		var stepsize = 25;
-		var time = 1000;
-		for (var i = 0; i < stepsize; i++) {
-			(function (j) {
-				setTimeout(e => vol.gain.value = j / stepsize, (stepsize - (i + 1)) * time / stepsize);
-			})(i);
-		}
-		setTimeout(e => osc.disconnect(), time);
-		setTimeout(e => vol.disconnect(), time);
+		var secs = 15;
+		vol.gain.exponentialRampToValueAtTime(1 / Number.MAX_SAFE_INTEGER, this.audio.currentTime + secs);
+		setTimeout(e => osc.disconnect(), secs * 1000);
+		setTimeout(e => vol.disconnect(), secs * 1000);
 	},
-	playHistory: function(i) { // should change recursion to a loop
-		if (i == undefined)
-			i = 0;
-		if (this.history.length >= 0) /* ??? */
-			this.note(this.history[i].key, false);
-		if (i < this.history.length - 1)
-			setTimeout(e => piano.playHistory(i + 1), this.history[i + 1].time);
+	playHistory: function(i) {
+		this.history.forEach(function(e) {
+			setTimeout(o => piano.note(e.key, false), e.time * 1000);
+		});
 	},
 	clearHistory: function() {
 		this.history = [];
-		this.lastTime = new Date().getTime();
+		this.initTime = this.audio.currentTime;
 	},
 	init: function() {
 		document.addEventListener("keydown", function(e) {
-			piano.note(e.key.length == 1 ? e.key.toLowerCase() : e.code, true);
-			if (e.key == " ")
+			var key = e.key.length == 1 ? e.key.toLowerCase() : e.code;
+			if (piano.keys.includes(key))
+				piano.note(key, true);
+			if (key == " ")
 				piano.clearHistory();
-			if (e.key == "Enter")
+			if (key == "Enter")
 				piano.playHistory();
 		});
 	},
 	record: [
-		[{"key":"ShiftRight","time":1408},{"key":"x","time":1961},{"key":"d","time":328},{"key":"c","time":303},{"key":".","time":8},{"key":"n","time":1984},{"key":"l","time":8},{"key":"x","time":1896},{"key":",","time":8},{"key":"b","time":1824},{"key":"k","time":24},{"key":"z","time":1968},{"key":"m","time":17},{"key":"v","time":2073},{"key":"z","time":1694},{"key":"ShiftLeft","time":360},{"key":"n","time":1984},{"key":"x","time":1},{"key":"k","time":1071},{"key":"d","time":8},{"key":"k","time":608},{"key":"c","time":16},{"key":"\\","time":1768},{"key":"x","time":1800},{"key":",","time":8},{"key":"b","time":1809},{"key":"k","time":1},{"key":"z","time":1886},{"key":"v","time":1744},{"key":"m","time":8},{"key":"ShiftLeft","time":1919},{"key":"c","time":3276},{"key":"m","time":344}],
-		[{"key":"x","time":311},{"key":"c","time":256},{"key":"k","time":272},{"key":"c","time":304},{"key":"k","time":264},{"key":"k","time":1752},{"key":"n","time":536},{"key":"c","time":1401},{"key":"k","time":287},{"key":"c","time":320},{"key":"k","time":496},{"key":"n","time":320},{"key":"b","time":704},{"key":"b","time":304},{"key":"g","time":400},{"key":"b","time":296},{"key":"b","time":312},{"key":"z","time":1168},{"key":"x","time":272},{"key":"d","time":288},{"key":"v","time":272},{"key":"b","time":296},{"key":"n","time":256},{"key":"m","time":304},{"key":",","time":271},{"key":".","time":305},{"key":".","time":1616},{"key":".","time":279},{"key":"k","time":537},{"key":"k","time":328},{"key":"b","time":673},{"key":"b","time":326},{"key":"c","time":609},{"key":"b","time":287},{"key":"c","time":617},{"key":"b","time":263},{"key":"n","time":504},{"key":"b","time":248},{"key":"n","time":257},{"key":"k","time":248},{"key":"b","time":335},{"key":"b","time":288},{"key":"c","time":545},{"key":"x","time":519},{"key":"b","time":3930},{"key":"v","time":225},{"key":"b","time":288},{"key":"m","time":247},{"key":"n","time":538},{"key":"v","time":535},{"key":"b","time":416},{"key":"n","time":559},{"key":"v","time":393},{"key":"ShiftRight","time":1607},{"key":"/","time":257},{"key":"m","time":1392},{"key":"n","time":263},{"key":"b","time":256},{"key":"n","time":256},{"key":"b","time":280},{"key":"v","time":504},{"key":"c","time":480},{"key":"v","time":216},{"key":"n","time":288},{"key":"b","time":520},{"key":"x","time":1552},{"key":"c","time":297},{"key":"v","time":279},{"key":"c","time":296},{"key":"n","time":832},{"key":"x","time":808},{"key":"c","time":3824},{"key":"j","time":256},{"key":".","time":296},{"key":"c","time":736},{"key":"k","time":240},{"key":".","time":256},{"key":"k","time":232},{"key":"b","time":288},{"key":"c","time":256},{"key":"b","time":248},{"key":"x","time":952},{"key":"c","time":297},{"key":"k","time":239},{"key":"n","time":248},{"key":"b","time":240},{"key":"n","time":271},{"key":"k","time":497},{"key":"b","time":496},{"key":".","time":504},{"key":"d","time":1160},{"key":".","time":256},{"key":"d","time":288},{"key":".","time":247},{"key":",","time":249},{"key":"m","time":264},{"key":",","time":256},{"key":".","time":256},{"key":"n","time":232},{"key":"m","time":240},{"key":"v","time":279},{"key":"x","time":465},{"key":"v","time":279},{"key":"b","time":1312},{"key":"n","time":233},{"key":"m","time":255},{"key":"n","time":457},{"key":"m","time":240},{"key":"n","time":271},{"key":"c","time":714},{"key":"v","time":238},{"key":"b","time":256},{"key":"b","time":249},{"key":"v","time":223},{"key":"c","time":336},{"key":"v","time":265},{"key":"b","time":272},{"key":"n","time":287},{"key":".","time":265},{"key":"/","time":511},{"key":"ShiftRight","time":472},{"key":"ShiftRight","time":464},{"key":".","time":1296},{"key":".","time":264},{"key":"/","time":1272},{"key":"/","time":264},{"key":"ShiftRight","time":960},{"key":"/","time":216},{"key":"Enter","time":720},{"key":"AltRight","time":61622},{"key":"MetaRight","time":7},{"key":"KeyI","time":184}],
-		[{"key":"ShiftRight","time":351},{"key":"x","time":1176},{"key":"d","time":265},{"key":"c","time":287},{"key":".","time":49},{"key":"n","time":1727},{"key":"l","time":8},{"key":",","time":1616},{"key":"x","time":16},{"key":"b","time":1680},{"key":"k","time":1},{"key":"m","time":1776},{"key":"z","time":1},{"key":"v","time":1798},{"key":"z","time":1424},{"key":"ShiftLeft","time":320},{"key":"x","time":1712},{"key":"n","time":8},{"key":"d","time":1080},{"key":"k","time":8},{"key":"c","time":584},{"key":"k","time":8},{"key":"\\","time":1664},{"key":",","time":1808},{"key":"x","time":7},{"key":"k","time":1865},{"key":"b","time":8},{"key":"z","time":1751},{"key":"v","time":1801},{"key":"m","time":7},{"key":"ShiftLeft","time":1784},{"key":"v","time":3421},{"key":",","time":240},{"key":"ShiftRight","time":319},{"key":"c","time":1497},{"key":"n","time":1720},{"key":"x","time":1832},{"key":",","time":8},{"key":"k","time":1800},{"key":"c","time":15},{"key":"x","time":1665},{"key":".","time":24},{"key":"d","time":4674},{"key":"c","time":272}],
+		[{"key":"x","time":0.5282539682539777},{"key":"c","time":0.7488435374149702},{"key":"k","time":0.9694331065759627},{"key":"c","time":1.2132426303854942},{"key":"k","time":1.4570521541950114},{"key":"k","time":2.7457596371882147},{"key":"n","time":3.239183673469398},{"key":"c","time":4.2724716553288005},{"key":"k","time":4.545306122448977},{"key":"c","time":4.806530612244899},{"key":"k","time":5.0619501133786855},{"key":"n","time":5.520544217687075},{"key":"b","time":5.973333333333343},{"key":"b","time":6.22875283446713},{"key":"g","time":6.449342403628123},{"key":"b","time":6.704761904761909},{"key":"b","time":6.965986394557831},{"key":"z","time":7.999274376417233},{"key":"x","time":8.23727891156463},{"key":"d","time":8.481088435374147},{"key":"v","time":8.701678004535154},{"key":"b","time":8.928072562358281},{"key":"n","time":9.125442176870749},{"key":"m","time":9.346031746031755},{"key":",","time":9.601451247165542},{"key":".","time":9.839455782312925},{"key":".","time":12.341405895691608},{"key":".","time":12.596825396825395},{"key":"k","time":13.136689342403628},{"key":"k","time":13.421133786848074},{"key":"b","time":13.879727891156463},{"key":"b","time":14.140952380952385},{"key":"c","time":14.59374149659864},{"key":"b","time":14.831746031746036},{"key":"c","time":15.29614512471656},{"key":"b","time":15.534149659863942},{"key":"n","time":15.766349206349204},{"key":"b","time":16.01596371882087},{"key":"n","time":16.39909297052155},{"key":"k","time":16.700952380952387},{"key":"b","time":16.96798185941043},{"key":"b","time":17.24081632653062},{"key":"x","time":17.8097052154195},{"key":"b","time":18.825578231292525},{"key":"v","time":19.063582766439907},{"key":"b","time":19.3364172335601},{"key":"m","time":19.58603174603175},{"key":"n","time":20.0736507936508},{"key":"b","time":20.54965986394558},{"key":"n","time":20.776054421768706},{"key":"v","time":21.037278911564627},{"key":"ShiftRight","time":23.44634920634921},{"key":"/","time":23.67274376417234},{"key":"m","time":25.309750566893428},{"key":"n","time":25.54195011337869},{"key":"b","time":25.785759637188207},{"key":"n","time":26.006349206349213},{"key":"b","time":26.261768707483},{"key":"v","time":26.807437641723354},{"key":"c","time":27.277641723356012},{"key":"v","time":27.509841269841274},{"key":"n","time":27.76526077097506},{"key":"b","time":28.25287981859411},{"key":"c","time":30.418140589569163},{"key":"v","time":30.632925170068035},{"key":"x","time":31.05668934240363},{"key":"c","time":31.28888888888889},{"key":"v","time":31.544308390022678},{"key":"c","time":31.846167800453514},{"key":"n","time":32.565986394557825},{"key":"x","time":33.32063492063493},{"key":"c","time":35.95029478458051},{"key":"k","time":36.18249433106577},{"key":".","time":36.437913832199555},{"key":"k","time":36.65269841269841},{"key":"b","time":36.884897959183675},{"key":"c","time":37.11709750566894},{"key":"b","time":37.33768707482993},{"key":"x","time":38.045895691609985},{"key":"c","time":38.2897052154195},{"key":"k","time":38.51029478458051},{"key":"n","time":38.765714285714296},{"key":"b","time":39.00952380952381},{"key":"n","time":39.270748299319735},{"key":"k","time":39.75836734693878},{"key":"b","time":40.25759637188209},{"key":".","time":40.762630385487526},{"key":"d","time":41.79011337868481},{"key":".","time":42.057142857142864},{"key":"d","time":42.27773242630386},{"key":".","time":42.53315192743764},{"key":",","time":42.776961451247175},{"key":"m","time":43.01496598639456},{"key":",","time":43.24716553287982},{"key":".","time":43.485170068027216},{"key":"n","time":43.73478458049887},{"key":"m","time":43.943764172335605},{"key":"v","time":44.21659863945578},{"key":"x","time":44.62875283446712},{"key":"v","time":44.9015873015873},{"key":"b","time":46.1902947845805},{"key":"n","time":46.39927437641724},{"key":"m","time":46.637278911564636},{"key":"n","time":47.12780045351474},{"key":"m","time":47.35129251700681},{"key":"n","time":47.615419501133786},{"key":"c","time":48.349750566893434},{"key":"v","time":48.57614512471656},{"key":"b","time":48.82575963718821},{"key":"b","time":49.06376417233561},{"key":"v","time":49.29015873015874},{"key":"c","time":49.55138321995466},{"key":"v","time":49.783582766439906},{"key":"b","time":50.0215873015873},{"key":"n","time":50.271201814058955},{"key":".","time":50.54984126984127},{"key":"/","time":51.07229024943311},{"key":"ShiftRight","time":51.5889342403628},{"key":"ShiftRight","time":52.11718820861678},{"key":".","time":53.405895691609985},{"key":".","time":53.69614512471655},{"key":"ShiftRight","time":55.031292517006804},{"key":"/","time":55.304126984126995}]
 	],
-	playRecord: function(i, j) { // should change recursion to a loop
-		if (j == undefined)
-			j = 0;
-		if (1) /* ??? */
-			this.note(this.record[i][j].key, false);
-		if (i < this.record[i].length - 1)
-			setTimeout(e => piano.playRecord(i, j + 1), this.record[i][j + 1].time);
+	playRecord: function(i, j) {
+		this.record[i].forEach(function(e) {
+			setTimeout(o => piano.note(e.key, false), e.time * 1000);
+		});
 	},
 	saveRecord: function() {
 		this.record.push(this.history);
