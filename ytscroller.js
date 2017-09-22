07c831321c91101de0d660c5725c0aa70a442636
Ted Michael Celis <krazete@gmail.com>
2017-09-21 17:20:40 -0700

    updated for new youtube layout

--- ytscroller.js
+++ ytscroller.js
@@ -1,25 +1,30 @@
-javascript:
 function ytscroller() {
-	var theater = document.getElementById("theater-background").getBoundingClientRect();
-	var masthead = document.getElementById("masthead-positioner");
-	var video = document.getElementById("player-api");
-	/* reset styles to accurately compute theater height */
-	masthead.removeAttribute("style");
-	video.removeAttribute("style");
-	if (document.body.scrollTop > theater.height) {
-		var sidebar = document.getElementById("watch7-sidebar").getBoundingClientRect();
-		var masthead_box = masthead.getBoundingClientRect();
-		var video_box = video.getBoundingClientRect();
-		/* get maximum possible video size without overlapping comments (unless window is too small) */
-		var widthRatio = Math.max(sidebar.width - 55, innerWidth - (sidebar.left - 55)) / video_box.width;
-		var flushRight = (innerWidth - video_box.right) / widthRatio;
-		masthead.style.zIndex = 999;
-		video.style.position = "fixed";
-		video.style.top = masthead_box.height + "px";
-		video.style.transform = "scale(" + widthRatio + ") translate(" + flushRight + "px)";
-		video.style.transformOrigin = "100% 0";
-		video.style.zIndex = 1000;
+	var masthead = document.getElementById("masthead-container").getBoundingClientRect();
+	var theater = document.getElementById("player-container").getBoundingClientRect();
+	var player = document.getElementById("movie_player");
+	var video = document.getElementsByTagName("video")[0];
+	var controls = document.getElementsByClassName("ytp-chrome-bottom")[0];
+	/* reset style to accurately compute theater height */
+	player.removeAttribute("style");
+	if (theater.bottom < masthead.height) {
+							var sidebar = document.getElementById("related").getBoundingClientRect();
+							var player_box = player.getBoundingClientRect();
+							var video_box = video.getBoundingClientRect();
+							/* get maximum possible video size without overlapping comments (unless window is too small) */
+							var widthRatio = Math.max(sidebar.width, innerWidth - sidebar.left) / video_box.width;
+							var flushRight = (innerWidth - video_box.right) / widthRatio;
+		player.style.position = "fixed";
+		player.style.top = masthead.height + "px";
+		player.style.right = "0px";
+		player.style.width = video_box.width + "px";
+		player.style.height = video_box.height + "px";
+							video.style.left = "0px";
+		controls.style.left = "12px";
+		controls.style.width = (video_box.width - 24) + "px";
+							player.style.transform = "scale(" + widthRatio + ")";
+							player.style.transformOrigin = "100% 0";
+							player.style.zIndex = 1000;
 	}
 }
-document.body.onscroll = ytscroller;
-document.body.onresize = ytscroller;
+window.addEventListener("scroll", ytscroller);
+window.addEventListener("resize", ytscroller);
