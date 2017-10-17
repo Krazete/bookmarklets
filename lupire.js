f76f88440c8adcca4d900599259e99b2c8e806eb
Ted Michael Celis <krazete@gmail.com>
2017-10-17 13:08:00 -0700

    init some old code (2015)

---
+++ lupire.js
@@ -0,0 +1,494 @@
+/* FUNCTIONS */
+// Choose random item from list.
+function choose(n){
+	return n[Math.floor(Math.random()*(n.length))];
+}
+
+// Choose random hex color, brightness range optional.
+function newHex(a,b){
+	if(typeof a=='undefined'){a=0;}
+	if(typeof b=='undefined'){b=16;}
+	var digits=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+	var hex='#';
+	for(var i=0;i<6;i++){
+		hex+=choose(digits.slice(a,b));
+	}
+	return hex;
+}
+
+// Choose random rgba color with given opacity, brightness range optional.
+function newByte(o,a,b){
+	if(typeof a=='undefined'){a=0;}
+	if(typeof b=='undefined'){b=256;}
+	var digits=[];
+	for(var i=0;i<256;i++){
+		digits.push(i);
+	}
+	var byte='rgba(';
+	for(i=0;i<3;i++){
+		byte+=choose(digits.slice(a,b))+',';
+	}
+	byte+=o+')';
+	return byte;
+}
+
+// Generate an obstacle with a specified distribution.
+function newBlock(p){
+	// Group group[i] has blocks[i].
+	var group=[0,1,2,2,3,3,3,3,4,4,5,5];
+	var sample=[];
+	for(var i=0;i<blocks.length;i++){
+		for(var j=0;j<p[group[i]];j++){
+			sample.push(blocks[i]);
+		}
+	}
+	return choose(sample);
+}
+
+/* BLOCKS */
+var blocks=[
+	full={img:1, goto:[0,1,2,3]},
+	
+	empty={img:0, goto:[2,3,0,1]},
+	
+	bslash={img:1, goto:[1,0,3,2]},
+	slash={img:0, goto:[3,2,1,0]},
+	
+	cornertr={img:[1,1,0,0], goto:[0,1,3,2]},
+	cornerbr={img:[1,0,0,1], goto:[0,2,1,3]},
+	cornerbl={img:[0,0,1,1], goto:[1,0,2,3]},
+	cornertl={img:[0,1,1,0], goto:[3,1,2,0]},
+	
+	vpipe={img:[1,0,1,0], goto:[0,3,2,1]},
+	hpipe={img:[0,1,0,1], goto:[2,1,0,3]},
+	
+	ccw={img:1, goto:[1,2,3,0]},
+	cw={img:0, goto:[3,0,1,2]}
+];
+
+// Stylesheet for spinners.
+var spinster=document.createElement('style');
+spinster.innerHTML='@keyframes cwise{ from{transform:rotate(0deg);} to{transform:rotate(360deg);} }';
+spinster.innerHTML+='@keyframes ccwise{ from{transform:rotate(360deg);} to{transform:rotate(0deg);} }';
+document.body.appendChild(spinster);
+
+/* SETTINGS */
+var span=3;
+var distribution=[0,2,1,0,0,0];
+var zoom=75;
+var unit=zoom/span;
+var speed=0.1;
+
+/* GAME BOARD */
+var game=document.createElement('div');
+game.style.position='fixed';
+game.style.top='50%';
+game.style.left='50%';
+game.style.transform='translate(-50%,-50%)';
+game.style.transition=speed+'s opacity';
+game.style.zIndex='9999';
+
+var compass;
+
+var corners=document.getElementsByClassName('cornerBox');
+var inners=document.getElementsByClassName('innerBox');
+var rawEdges=document.getElementsByClassName('edgeBox');
+var edges;
+
+var goal;
+var ball;
+
+var lvl=1;
+var pts=0;
+var hp=15;
+
+var board=document.createElement('div');
+board.style.background='white';
+board.style.textAlign='center';
+board.style.position='fixed';
+board.style.top=(100+zoom)/2+'vmin';
+board.style.left='50%';
+board.style.transform='translateX(-50%)';
+board.style.transition=speed+'s opacity';
+board.style.border='1px solid black';
+board.style.padding='1vmin';
+board.style.borderRadius='1vmin';
+board.style.zIndex='9999';
+document.body.appendChild(board);
+
+var tab=' &shy; &shy; &shy; &shy; &shy; &shy; &shy; &shy; &shy; ';
+
+// Refreshes game with new settings.
+function newGame(s,d,z,v){
+	if(typeof s=='undefined'){s=span;}
+	span=s;
+	if(typeof d=='undefined'){d=distribution;}
+	distribution=d;
+	if(typeof z=='undefined'){z=zoom;}
+	zoom=z;
+	unit=zoom/span;
+	if(typeof v=='undefined'){v=speed;}
+	speed=v;
+	
+	// Updates board.
+	board.style.opacity=1;
+	board.innerHTML='Lives: '+hp+tab+'Points: '+pts+tab+'Level: '+(span-2)+tab+'Sublevel: '+lvl+'/'+(span-2)+'</br>';
+	
+	if(hp==0 || span==2){
+		hide();
+		disable();
+		ball.remove();
+		board.innerHTML+='GAME OVER';
+		return 0;
+	}
+	
+	// Updates gameboard.
+	while(game.lastChild){
+		game.removeChild(game.lastChild);
+	}
+	game.remove();
+		
+	game.style.background=newByte(3/4,64,192);
+	game.style.height=zoom+'vmin';
+	game.style.width=zoom+'vmin';
+	game.style.borderRadius=1/2*unit+'vmin';
+	game.style.opacity=1;
+	document.body.appendChild(game);
+	
+	compass=newBoard();
+		
+	edges=[];
+	for(i=0;i<rawEdges.length;i++){
+		rawEdges[i].end=newPath(rawEdges[i].x,rawEdges[i].y);
+		edges.push(rawEdges[i]);
+	}
+	edges.sort(function(a,b){ return a.end.steps-b.end.steps; })
+	
+	setTimeout(setUp,10000*speed*(1+span/10));
+}
+
+// Starts game.
+function setUp(){
+	goal=newGoal();
+	ball=newBall(goal.x,goal.y);
+	hide();
+	enable();
+}
+
+// Creates a new randomized gameboard.
+function newBoard(){
+	var rcolor=newHex(0,8);
+	var boxes=[];
+	var map=[];
+	for(var i=0;i<span;i++){
+		boxes[i]=[];
+		map[i]=[];
+		for(var j=0;j<span;j++){
+			// Creates background grid.
+			var grid=document.createElement('div');
+			grid.style.height=unit+'vmin';
+			grid.style.width=unit+'vmin';
+			grid.style.position='absolute';
+			grid.style.top=i*unit+'vmin';
+			grid.style.left=j*unit+'vmin';
+			grid.style.boxSizing='border-box';
+			grid.style.boxShadow='inset 0 0 3vmin '+game.style.backgroundColor;
+			game.appendChild(grid);
+			
+			// Creates obstacles.
+			var box=document.createElement('div');
+			box.x=i;
+			box.y=j;
+			box.style.position='absolute';
+			box.style.top=i*unit+'vmin';
+			box.style.left=j*unit+'vmin';
+			box.style.boxSizing='border-box';
+			if((i==0 && j==0) || (i==0 && j==span-1) || (i==span-1 && j==span-1) || (i==span-1 && j==0)){
+				// Removes corner grids.
+				grid.remove();
+				
+				boxes[i][j]=blocks[0];
+				map[i][j]='X';
+				box.className='cornerBox';
+				box.style.transition=speed+'s border-radius';
+				
+				//Specifies function of each corner block.
+				if(i==0 && j==0){
+					box.style.borderTopLeftRadius='50%';
+					box.setAttribute('onMouseOver','this.style.borderTopLeftRadius=\'25%\'');
+					box.setAttribute('onMouseOut','this.style.borderTopLeftRadius=\'50%\'');
+					box.setAttribute('onClick','newGame()');
+				}
+				else if(i==0 && j==span-1){
+					var exit1=document.createElement('div');
+					exit1.style.background='white';
+					exit1.style.height=1/2*unit+'vmin';
+					exit1.style.width=1/10*unit+'vmin';
+					exit1.style.borderRadius=100/3+'%';
+					exit1.style.position='absolute';
+					exit1.style.top=1/4*unit+'vmin';
+					exit1.style.left=9/20*unit+'vmin';
+					exit1.style.transform='rotate(45deg)';
+					box.appendChild(exit1);
+					var exit2=exit1.cloneNode();
+					exit2.style.transform='rotate(-45deg)';
+					box.appendChild(exit2);
+					
+					box.style.borderTopRightRadius='50%';
+					box.setAttribute('onMouseOver','this.style.borderTopRightRadius=\'25%\'');
+					box.setAttribute('onMouseOut','this.style.borderTopRightRadius=\'50%\'');
+					box.setAttribute('onClick','game.style.opacity=0;board.style.opacity=0;setTimeout(function(){ game.remove(); board.remove(); },100)');
+				}
+				else if(i==span-1 && j==span-1){
+					box.style.borderBottomRightRadius='50%';
+					box.setAttribute('onMouseOver','this.style.borderBottomRightRadius=\'25%\'');
+					box.setAttribute('onMouseOut','this.style.borderBottomRightRadius=\'50%\'');
+					box.setAttribute('onClick','hide()');
+				}
+				else if(i==span-1 && j==0){
+					box.style.borderBottomLeftRadius='50%';
+					box.setAttribute('onMouseOver','this.style.borderBottomLeftRadius=\'25%\'');
+					box.setAttribute('onMouseOut','this.style.borderBottomLeftRadius=\'50%\'');
+					box.setAttribute('onClick','show()');
+				}
+			}
+			else if(i==0 || i==span-1 || j==0 || j==span-1){
+				// Sets up edges.
+				if(i==0 || i==span-1){
+					boxes[i][j]=blocks[8];
+					map[i][j]=(i==0 ? [3,3,3,3] : [1,1,1,1]);
+				}
+				else if(j==0 || j==span-1){
+					boxes[i][j]=blocks[9];
+					map[i][j]=(j==0 ? [0,0,0,0] : [2,2,2,2]);
+				}
+				box.className='edgeBox';
+				box.style.background='rgba(0,0,0,0.25)';
+				box.style.transition=2*speed+'s background';
+			}
+			else{
+				// Sets up inner obstacles.
+				boxes[i][j]=newBlock(distribution);
+				map[i][j]=boxes[i][j].goto;
+				box.className='innerBox';
+				box.style.borderRadius=100/3+'%';
+				box.style.transition=2*speed+'s opacity';
+			}
+			
+			// Sets shorthand for boxes[i][j].
+			var b=boxes[i][j];
+			var color=box.className=='innerBox' ? 'white' : rcolor;
+			
+			// Sets style for each type of block.
+			if(b==full || b==empty){
+				box.style.background=(b.img ? color : 'transparent');
+				box.style.height=unit+'vmin';
+				box.style.width=unit+'vmin';
+			}
+			else if(b==bslash || b==slash){
+				box.style.background=color;
+				box.style.left=(j+2/5)*unit+'vmin';
+				box.style.height=unit+'vmin';
+				box.style.width=1/5*unit+'vmin';
+				box.style.transform='rotate('+[45,-45][b.img]+'deg)';
+			}
+			else if(b==cornertr || b==cornerbr || b==cornerbl || b==cornertl){
+				box.style.height=unit+'vmin';
+				box.style.width=unit+'vmin';
+				box.style.borderRight=1/2*unit+'vmin solid '+(b.img[0] ? color : 'transparent');
+				box.style.borderTop=1/2*unit+'vmin solid '+(b.img[1] ? color : 'transparent');
+				box.style.borderLeft=1/2*unit+'vmin solid '+(b.img[2] ? color : 'transparent');
+				box.style.borderBottom=1/2*unit+'vmin solid '+(b.img[3] ? color : 'transparent');
+			}
+			else if(b==vpipe || b==hpipe){
+				box.style.height=unit+'vmin';
+				box.style.width=unit+'vmin';
+				box.style.borderRight=1/5*unit+'vmin solid '+(b.img[0] ? color : 'transparent');
+				box.style.borderTop=1/5*unit+'vmin solid '+(b.img[1] ? color : 'transparent');
+				box.style.borderLeft=1/5*unit+'vmin solid '+(b.img[2] ? color : 'transparent');
+				box.style.borderBottom=1/5*unit+'vmin solid '+(b.img[3] ? color : 'transparent');				
+			}
+			else if(b==ccw || b==cw){
+				box.style.background=color;
+				box.style.left=(j+2/5)*unit+'vmin';
+				box.style.height=unit+'vmin';
+				box.style.width=1/5*unit+'vmin';
+				box.style.animation=['cwise','ccwise'][b.img]+' '+5*speed+'s infinite linear';
+			}
+			else{
+				console.log('Error: Unknown block entered into display().');
+			}
+			game.appendChild(box);
+		}
+	}
+	// Returns course directions for each block.
+	return map;
+}
+
+// Gives all edges their path information.
+function newPath(x,y,z,steps){
+	if(typeof z=='undefined'){z=compass[x][y][0];}
+	if(typeof steps=='undefined'){steps=0;}
+	var to=compass[x][y][z];
+	var next=[[0,1],[-1,0],[0,-1],[1,0]];
+	x+=next[to][0];
+	y+=next[to][1];
+	if(x!=0 && x!=span-1 && y!=0 && y!=span-1){
+		return newPath(x,y,(to+2)%4,(compass[x][y]==empty.goto ? steps+1 : steps+2));
+	}
+	else{
+		return {x:x,y:y,steps:steps};
+	}
+}
+
+/* BALL */
+// Creates a new ball at given coordinates.
+function newBall(x,y){
+	var ball=document.createElement('div');
+	ball.style.background='white';
+	ball.style.position='absolute';
+	ball.style.top=x*unit+'vmin';
+	ball.style.left=y*unit+'vmin';
+	ball.style.width=2/5*unit+'vmin';
+	ball.style.height=2/5*unit+'vmin';
+	ball.style.borderRadius='50%';
+	ball.style.margin=3/10*unit+'vmin';
+	ball.style.transition=speed+'s top, '+speed+'s left, '+speed+'s background';
+	ball.style.transitionTimingFunction='linear';
+	ball.style.pointerEvents='none';
+	game.appendChild(ball);
+	return ball;
+}
+
+// Moves the ball to its path end, where it decides settings for the new gameboard.
+function move(h,k,x,y,z){
+	if(typeof z=='undefined'){z=compass[x][y][0];}
+	
+	if(x>0 && x<span-1 && y>0 && y<span-1){
+		getByXY(x,y).style.opacity=1;
+	}
+	
+	var to=compass[x][y][z];
+	var next=[[0,1],[-1,0],[0,-1],[1,0]];
+	x+=next[to][0];
+	y+=next[to][1];
+	ball.style.top=unit*x+'vmin';
+	ball.style.left=unit*y+'vmin';
+	if(x!=0 && x!=span-1 && y!=0 && y!=span-1){
+		setTimeout(function(){ move(h,k,x,y,(to+2)%4); },1000*speed);
+	}
+	else{
+		// If correct.
+		if(goal.end.x==h && goal.end.y==k){
+			// Make green.
+			for(var i=0;i<edges.length;i++){
+				edges[i].style.background='rgba(0,128,0,0.5)';
+			}
+			ball.style.background='green';
+			// Heighten hp and lvl.
+			pts+=goal.end.steps;
+			if(lvl==span-2){
+				span++;
+				lvl=1;
+			}
+			else{
+				lvl++;
+			}
+		}
+		// If incorrect.
+		else{
+			// Make red.
+			for(var i=0;i<edges.length;i++){
+				edges[i].style.background='rgba(128,0,0,0.5)';
+			}
+			ball.style.background='red';
+			// Lower hp and lvl.
+			hp--;
+			if(span>3){
+				if(lvl==1){
+					span--;
+					lvl=span-2;
+				}
+				else{
+					lvl--;
+				}
+			}
+		}
+		// End current gameboard.
+		setTimeout(show,10000*speed);
+		setTimeout(newGame,20000*speed);
+	}
+}
+
+// Retrieves an inner block by its (x,y) coordinates.
+function getByXY(x,y){
+for(var i=0;i<inners.length;i++){
+	if(inners[i].x==x && inners[i].y==y){
+		return inners[i];
+	}
+}
+}
+
+// Hides all inner blocks.
+function hide(){
+	for(var i=0;i<inners.length;i++){
+		inners[i].style.opacity=0;
+	}
+}
+
+// Shows all inner blocks.
+function show(){
+	for(var i=0;i<inners.length;i++){
+		inners[i].style.opacity=1;
+	}
+}
+
+// Picks the ball starting point based on path length.
+function newGoal(){
+	var goals={};
+	for(var i=0;i<edges.length;i++){
+		/*
+		if(edges[i].end.steps>(Math.pow(span,2))/2){
+			break;
+		}
+		*/
+		if(goals[edges[i].end.steps]==undefined){
+			goals[edges[i].end.steps]=[edges[i]];
+		}
+		else{
+			goals[edges[i].end.steps].push(edges[i]);
+		}
+	}
+	key=Object.keys(goals);
+	return choose(goals[key[key.length-1]]);
+	//return choose(goals[key[Math.floor(key.length*lvl/(span-1))]]);
+}
+
+// Make edges clickable.
+function enable(){
+	for(var i=0;i<edges.length;i++){
+		edges[i].style.background='transparent';
+		edges[i].setAttribute('onMouseOver','this.style.background=\'rgba(0,0,0,0.25)\'');
+		edges[i].setAttribute('onMouseOut','this.style.background=\'transparent\'');
+		edges[i].setAttribute('onClick','guess('+goal.x+','+goal.y+','+edges[i].x+','+edges[i].y+')');
+	}
+}
+
+// Remove edge clickability.
+function disable(){
+	for(var i=0;i<edges.length;i++){
+		edges[i].style.background='rgba(0,0,0,0.25)';
+		edges[i].removeAttribute('onMouseOver');
+		edges[i].removeAttribute('onMouseOut');
+		edges[i].removeAttribute('onClick');
+	}
+}
+
+// Start the ball with your prospective destination.
+function guess(x,y,h,k){
+	disable();
+	move(h,k,x,y);
+}
+
+// Begin.
+newGame();
---
+++ tic.js
@@ -0,0 +1,231 @@
+// Randomize the first marker.
+var turn=Math.random()<1/2 ? 0 : 1;
+
+// Set unit size.
+var unit=100/11;
+
+// Create menu.
+var menu=document.createElement('div');
+menu.style.position='fixed';
+menu.style.top='50%';
+menu.style.left='50%';
+menu.style.height=9*unit+'vmin';
+menu.style.width=9*unit+'vmin';
+menu.style.transform='translate(-50%,-50%)';
+menu.style.zIndex=9999;
+menu.innerHTML='<a style=\'background:white;position:absolute;left:100%;cursor:pointer\' onClick=\'menu.remove()\'>exit</a>';
+document.body.appendChild(menu);
+
+// Create board.
+var board=document.createElement('div');
+board.style.background='rgba(255,255,255,0.9)';
+board.style.position='absolute';
+board.style.top=0;
+board.style.left=0;
+board.style.height=9*unit+'vmin';
+board.style.width=9*unit+'vmin';
+board.mark=null;
+menu.appendChild(board);
+
+// Create macro and micro maps.
+BOXES=[];
+boxes=[[],[],[],[],[],[],[],[],[]];
+
+// Fill the board.
+for(var i=0;i<3;i++){
+	for(var j=0;j<3;j++){
+		var BOX=document.createElement('div');				
+		BOX.style.position='absolute';
+		BOX.style.top=i*100/3+'%';
+		BOX.style.left=j*100/3+'%';
+		BOX.style.height=100/3+'%';
+		BOX.style.width=100/3+'%';
+		BOX.mark=null;
+		
+		for(var k=0;k<3;k++){
+			for(var l=0;l<3;l++){
+				var box=document.createElement('div');
+				
+				box.setAttribute('onMouseOver','this.style.background=\'rgba(0,0,0,0.25)\'');
+				box.setAttribute('onMouseOut','this.style.background=\'transparent\'');
+				box.setAttribute('onClick','pick(this)');
+				
+				box.style.position='absolute';
+				box.style.top=k*100/3+'%';
+				box.style.left=l*100/3+'%';
+				box.style.height=100/3+'%';
+				box.style.width=100/3+'%';
+				
+				box.macro=3*i+j;
+				box.micro=3*k+l;
+				box.mark=null;
+				boxes[3*i+j].push(box);
+				
+				if(box.macro%2==0){
+					box.style.boxShadow='0 0 1vmin black inset';
+				}
+				else{
+					box.style.boxShadow='0 0 1vmin gray inset';
+				}
+				
+				BOX.appendChild(box);
+			}
+		}
+		BOXES.push(BOX);
+		board.appendChild(BOX);
+	}
+}
+
+/*
++---+---+---+
+| 0 | 1 | 2 |
++---+---+---+
+| 3 | 4 | 5 |
++---+---+---+
+| 6 | 7 | 8 |
++---+---+---+
+*/
+
+// Check for victories.
+function check(m){
+	var checkmark=0;
+	if(m[0].mark!=null){
+		if((m[0].mark==m[1].mark && m[1].mark==m[2].mark) || (m[0].mark==m[3].mark && m[3].mark==m[6].mark)){
+			checkmark=1;
+		}
+	}
+	if(m[4].mark!=null){
+		if((m[3].mark==m[4].mark && m[4].mark==m[5].mark) || (m[1].mark==m[4].mark && m[4].mark==m[7].mark) || (m[0].mark==m[4].mark && m[4].mark==m[8].mark) || (m[2].mark==m[4].mark && m[4].mark==m[6].mark)){
+			checkmark=1;
+		}
+	}
+	if(m[8].mark!=null){
+		if((m[6].mark==m[7].mark && m[7].mark==m[8].mark) || (m[2].mark==m[5].mark && m[5].mark==m[8].mark)){
+			checkmark=1;
+		}
+	}
+	
+	if(checkmark){
+		marker(m[0].parentNode);
+	}
+	else{
+		if(m[0].mark!=null && m[1].mark!=null && m[2].mark!=null && m[3].mark!=null && m[4].mark!=null && m[5].mark!=null && m[6].mark!=null && m[7].mark!=null && m[8].mark!=null){
+			draw(m[0].parentNode);
+		}
+	}
+}
+
+// Place a tied marker.
+function draw(obj){
+	// Disable the tied box.
+	obj.mark=2;
+	disable(obj);
+	obj.style.pointerEvents='none';
+}
+
+// Place a marker.
+function marker(obj){
+	// Place an O.
+	if(turn==0){
+		var o=document.createElement('div');
+		o.style.boxSizing='border-box';
+		o.style.position='absolute';
+		o.style.top='10%';
+		o.style.left='10%';
+		o.style.height='80%';
+		o.style.width='80%';
+		// Set O border size based on nested box level, because for some reason percentage isn't supported.
+		var inBOXES=0;
+		for(var i=0;i<9;i++){
+			if(obj==BOXES[i]){
+				inBOXES=1;
+				break;
+			}
+		}
+		o.style.border=unit*(obj==board ? 9 : (inBOXES ? 3 : 1))/5+'vmin solid black';
+		o.style.borderRadius='50%';
+		obj.appendChild(o);
+	}
+	// Place an X.
+	else{
+		var x1=document.createElement('div');
+		x1.style.boxSizing='border-box';
+		x1.style.background='black';
+		x1.style.position='absolute';
+		x1.style.top='10%';
+		x1.style.left='40%';
+		x1.style.height='80%';
+		x1.style.width='20%';
+		x1.style.transform='rotate(45deg)';
+		obj.appendChild(x1);
+		var x2=x1.cloneNode();
+		x2.style.transform='rotate(-45deg)';
+		obj.appendChild(x2);
+	}
+	// Mark the map and disable the marked box.
+	obj.mark=turn;
+	disable(obj);
+	obj.style.pointerEvents='none';
+}
+
+// Do stuff to a box upon click.
+function pick(b){
+	// Mark box.
+	marker(b);
+	
+	// Check victories.
+	check(boxes[b.macro]);
+	check(BOXES);
+	
+	// Pass turn to other player.
+	turn>0 ? turn=0 : turn=1;
+	
+	// Enable next macro box if it isn't won.
+	if(BOXES[b.micro].mark==null){
+		// Disable all boxes.
+		for(i=0;i<9;i++){
+			for(var j=0;j<9;j++){
+				disable(boxes[i][j]);
+			}
+		}
+		// Enable next macro box.
+		for(i=0;i<9;i++){
+			// Forces won macro boxes to stay disabled.
+			if(board.mark==null){
+				enable(boxes[b.micro][i]);
+			}
+		}
+	}
+	// Enable all macro boxes if it's won.
+	else{
+		for(i=0;i<9;i++){
+			for(var j=0;j<9;j++){
+				// Forces won macro boxes to stay disabled.
+				if(board.mark==null){
+					enable(boxes[i][j]);
+				}
+			}
+		}
+	}
+	// Forces won micro boxes to stay disabled.
+	disable(b);
+}
+
+// Disables a box.
+function disable(b){
+	b.style.background='rgba(0,0,0,0.25)';
+	b.setAttribute('onMouseOver','');
+	b.setAttribute('onMouseOut','');
+	b.setAttribute('onClick','');
+}
+
+// Enables a box.
+function enable(b){
+	// Forces won boxes to stay disabled.
+	if(b.mark==null){
+		b.style.background='transparent';
+	}
+	b.setAttribute('onMouseOver','this.style.background=\'rgba(0,0,0,0.25)\'');
+	b.setAttribute('onMouseOut','this.style.background=\'transparent\'');
+	b.setAttribute('onClick','pick(this)');
+}
