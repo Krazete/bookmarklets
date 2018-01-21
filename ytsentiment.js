dccbe51993deb73760b4467d0374aeeb78e72b9a
Ted Michael Celis <krazete@gmail.com>
2018-01-20 18:41:26 -0800

    the color now remains and updates when the thumbnail link is updated

--- ytsentiment.js
+++ ytsentiment.js
@@ -1,21 +1,28 @@
-Array.from(document.getElementsByTagName("ytd-video-renderer"))
-.concat(Array.from(document.getElementsByTagName("ytd-grid-video-renderer")))
-.concat(Array.from(document.getElementsByTagName("ytd-compact-video-renderer")))
-.forEach(function(e) {
-	var link = e.querySelector("#thumbnail").href;
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", link, true);
-	xhr.onload = function() {
-		var match = this.responseText.match(/"percentIfIndifferent":(\d+),/);
-		var rating = parseInt(Math.pow(match[1] / 100, Math.E) * 256);
-		e.style.background = "rgb(" + (256 - rating) + ", " + rating + ", 0)";
-		setTimeout(function() {
-			e.style.transition = "background 1s";
-			e.style.background = "";
-		}, 10000);
-		setTimeout(function() {
-			e.style.transition = "";
-		}, 1000);
-    };
-	xhr.send();
-});
+function empath() {
+	setTimeout(empath, 1000);
+	Array.from(document.getElementsByTagName("ytd-thumbnail"))
+	.forEach(function(e) {
+		var oldLink = e.getAttribute("value");
+		var newLink = e.children[0].href;
+		if (oldLink != newLink) {
+			e.setAttribute("value", newLink);
+			e.style.border = "";
+
+			var xhr = new XMLHttpRequest();
+			xhr.open("GET", newLink, true);
+			xhr.onload = function() {
+				var match = this.responseText.match(/"percentIfIndifferent":(\d+)/);
+				var linRating = parseInt(match[1]);
+				if (linRating == 0) {
+					e.style.border = "1px solid gray";
+				}
+				else {
+					var logRating = parseInt(Math.pow(linRating / 100, Math.E) * 256);
+					e.style.border = "1px solid rgb(" + (256 - logRating) + ", " + logRating + ", 0)";
+				}
+		    };
+			xhr.send();
+		}
+	});
+}
+empath();
