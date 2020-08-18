a945e2fb982fdf2abc9c2da13ca4c124e5fb4377
Ted Michael Celis <krazete@gmail.com>
2020-08-17 21:52:19 -0700

    Update pop.js

--- pop.js
+++ pop.js
@@ -1,15 +1,15 @@
 javascript:
-if (!video) {
-	var video = document.getElementsByTagName("video")[0];
-	function popout() {
-		video.disablePictureInPicture = false;
-		video.requestPictureInPicture();
-	}
-	function popin() {
-		video.disablePictureInPicture = true;
-	}
-	video.addEventListener("enterpictureinpicture", e => pop = popin);
-	video.addEventListener("leavepictureinpicture", e => pop = popout);
-	popout();
+var video = document.getElementsByTagName("video")[0];
+var pop;
+function popout() {
+	video.disablePictureInPicture = false;
+	video.requestPictureInPicture();
 }
+function popin() {
+	video.disablePictureInPicture = true;
+}
+video.addEventListener("enterpictureinpicture", e => pop = popin);
+video.addEventListener("leavepictureinpicture", e => pop = popout);
+
+pop ? void() : pop = popout;
 pop();
