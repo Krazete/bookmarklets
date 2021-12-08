909be6e5ccb315a7ba4c1da6b23cfa5db66cf2dd
Kian <kian@reconventures.com>
2021-12-08 11:41:22 -0800

    Update tic.js
    Comments dont work in bookmarklets

--- tic.js
+++ tic.js
@@ -1,10 +1,8 @@
-// Randomize the first marker.
+
 var turn=Math.random()<1/2 ? 0 : 1;
 
-// Set unit size.
 var unit=100/11;
 
-// Create menu.
 var menu=document.createElement('div');
 menu.style.position='fixed';
 menu.style.top='50%';
@@ -16,7 +14,6 @@ menu.style.zIndex=9999;
 menu.innerHTML='<a style=\'background:white;position:absolute;left:100%;cursor:pointer\' onClick=\'menu.remove()\'>exit</a>';
 document.body.appendChild(menu);
 
-// Create board.
 var board=document.createElement('div');
 board.style.background='rgba(255,255,255,0.9)';
 board.style.position='absolute';
@@ -27,11 +24,9 @@ board.style.width=9*unit+'vmin';
 board.mark=null;
 menu.appendChild(board);
 
-// Create macro and micro maps.
 BOXES=[];
 boxes=[[],[],[],[],[],[],[],[],[]];
 
-// Fill the board.
 for(var i=0;i<3;i++){
 	for(var j=0;j<3;j++){
 		var BOX=document.createElement('div');				
@@ -86,7 +81,6 @@ for(var i=0;i<3;i++){
 +---+---+---+
 */
 
-// Check for victories.
 function check(m){
 	var checkmark=0;
 	if(m[0].mark!=null){
@@ -115,7 +109,6 @@ function check(m){
 	}
 }
 
-// Place a tied marker.
 function draw(obj){
 	// Disable the tied box.
 	obj.mark=2;
@@ -123,9 +116,8 @@ function draw(obj){
 	obj.style.pointerEvents='none';
 }
 
-// Place a marker.
 function marker(obj){
-	// Place an O.
+	
 	if(turn==0){
 		var o=document.createElement('div');
 		o.style.boxSizing='border-box';
@@ -134,7 +126,7 @@ function marker(obj){
 		o.style.left='10%';
 		o.style.height='80%';
 		o.style.width='80%';
-		// Set O border size based on nested box level, because for some reason percentage isn't supported.
+		
 		var inBOXES=0;
 		for(var i=0;i<9;i++){
 			if(obj==BOXES[i]){
@@ -146,7 +138,7 @@ function marker(obj){
 		o.style.borderRadius='50%';
 		obj.appendChild(o);
 	}
-	// Place an X.
+	
 	else{
 		var x1=document.createElement('div');
 		x1.style.boxSizing='border-box';
@@ -162,41 +154,40 @@ function marker(obj){
 		x2.style.transform='rotate(-45deg)';
 		obj.appendChild(x2);
 	}
-	// Mark the map and disable the marked box.
+	
 	obj.mark=turn;
 	disable(obj);
 	obj.style.pointerEvents='none';
 }
 
-// Do stuff to a box upon click.
 function pick(b){
-	// Mark box.
+	
 	marker(b);
 	
-	// Check victories.
+	
 	check(boxes[b.macro]);
 	check(BOXES);
 	
-	// Pass turn to other player.
+	
 	turn>0 ? turn=0 : turn=1;
 	
-	// Enable next macro box if it isn't won.
+	
 	if(BOXES[b.micro].mark==null){
-		// Disable all boxes.
+		
 		for(i=0;i<9;i++){
 			for(var j=0;j<9;j++){
 				disable(boxes[i][j]);
 			}
 		}
-		// Enable next macro box.
+		
 		for(i=0;i<9;i++){
-			// Forces won macro boxes to stay disabled.
+			
 			if(board.mark==null){
 				enable(boxes[b.micro][i]);
 			}
 		}
 	}
-	// Enable all macro boxes if it's won.
+	
 	else{
 		for(i=0;i<9;i++){
 			for(var j=0;j<9;j++){
@@ -207,11 +198,11 @@ function pick(b){
 			}
 		}
 	}
-	// Forces won micro boxes to stay disabled.
+	
 	disable(b);
 }
 
-// Disables a box.
+
 function disable(b){
 	b.style.background='rgba(0,0,0,0.25)';
 	b.setAttribute('onMouseOver','');
@@ -219,9 +210,7 @@ function disable(b){
 	b.setAttribute('onClick','');
 }
 
-// Enables a box.
 function enable(b){
-	// Forces won boxes to stay disabled.
 	if(b.mark==null){
 		b.style.background='transparent';
 	}
