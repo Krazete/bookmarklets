877a60b915d886beef1197d9e5c80beb455d41ae
Ted Michael Celis <krazete@gmail.com>
2023-06-04 19:10:47 -0700

    prioritize unpaused videos

--- pop.js
+++ pop.js
@@ -1,4 +1,11 @@
-var video = document.getElementsByTagName("video")[0];
+var videos = Array.from(document.getElementsByTagName("video"));
+var video = videos[0];
+for (var v of videos) {
+	if (!v.paused) {
+		video = v;
+		break;
+	}
+}
 var pop;
 function popout() {
 	video.removeAttribute("disablePictureInPicture");
